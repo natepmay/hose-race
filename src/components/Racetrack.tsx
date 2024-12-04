1b071b801c56ed92eacccc0a40afb59c4793443f
Nate May <nathaniel.p.may@gmail.com>
2024-12-03 23:06:04 -0800

    Actual little hoses

--- src/components/Racetrack.tsx
+++ src/components/Racetrack.tsx
@@ -1,5 +1,5 @@
 import "../App.css";
-import { Marker } from "./Marker";
+// import { Marker } from "./Marker";
 
 export function Racetrack({
   name,
@@ -8,19 +8,23 @@ export function Racetrack({
   name: string;
   progress: number;
 }) {
-  const markerStates = [];
-  for (let i = 0; i < 15; i++) {
-    markerStates.push(i <= progress - 1);
-  }
+  // const markerStates = [];
+  // for (let i = 0; i < 15; i++) {
+  //   markerStates.push(i <= progress - 1);
+  // }
 
-  const markers = markerStates.map((state, i) => {
-    return <Marker isOn={state} key={i}></Marker>;
-  });
+  // const markers = markerStates.map((state, i) => {
+  //   return <Marker isOn={state} key={i}></Marker>;
+  // });
+  const topY = progress * 50 - 100;
 
   return (
     <div className="racetrack">
       {name}
-      {markers}
+      <img
+        src="./src/assets/hose.png"
+        style={{ position: "relative", width: "5px", top: topY, left: 0 }}
+      ></img>
     </div>
   );
 }
