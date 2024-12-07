862825712111b5d490de2c227e5944be2ba087cc
natepmay <nathaniel.p.may@gmail.com>
2024-12-07 08:44:52 -0800

    Replace with design from codepen

--- src/App.tsx
+++ src/App.tsx
@@ -50,8 +50,84 @@ function App() {
     );
   });
   return (
-    <div className="main">
-      <div className="racetracks">{racetracks}</div>
+    <div className="parent">
+      <div className="header">
+        <div className="header-text">
+          <div>Hose Race</div>
+          <div>3010</div>
+        </div>
+      </div>
+      <div className="all-racetrack-content">
+        <div className="lines">
+          <div className="full-line line1">
+            <div className="line-label">0</div>
+            <div className="line"></div>
+          </div>
+          <div className="full-line line2">
+            <div className="line-label">25</div>
+            <div className="line"></div>
+          </div>
+          <div className="full-line line3">
+            <div className="line-label">50</div>
+            <div className="line"></div>
+          </div>
+          <div className="full-line line4">
+            <div className="line-label">75</div>
+            <div className="line"></div>
+          </div>
+          <div className="full-line finish">
+            <div className="line-label">100</div>
+            <div className="line"></div>
+          </div>
+        </div>
+        <div className="tracks">
+          <div className="racetrack-container">
+            <div className="racetrack">
+              <div className="track-head">
+                <div className="track-word">word1</div>
+                <div className="track-score">34</div>
+              </div>
+              <div className="track-main"></div>
+              <div className="post-light on"></div>
+            </div>
+            <div className="racetrack">
+              <div className="track-head">
+                <div className="track-word">word2</div>
+                <div className="track-score">34</div>
+              </div>
+              <div className="track-main"></div>
+              <div className="post-light on"></div>
+            </div>
+            <div className="racetrack">
+              <div className="track-head">
+                <div className="track-word">word3</div>
+                <div className="track-score">34</div>
+              </div>
+              <div className="track-main"></div>
+              <div className="post-light on"></div>
+            </div>
+            <div className="racetrack">
+              <div className="track-head">
+                <div className="track-word">word4</div>
+                <div className="track-score">34</div>
+              </div>
+              <div className="track-main"></div>
+              <div className="post-light on"></div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="post-card-slot">
+        <div className="post-card">
+          And that's when I turned to him and I was like what did u say word1?
+        </div>
+      </div>
+      <div className="footer">
+        <div className="footer-text">
+          <div>by Nate May</div>
+          <div>[GH]</div>
+        </div>
+      </div>
     </div>
   );
 }
