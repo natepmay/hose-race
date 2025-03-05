a3fcea6a70813c7cbacdb9a98a7b1bba73b2ec09
Nate May <nathaniel.p.may@gmail.com>
2025-03-05 14:28:40 -0800

    Animate hose motion

--- src/components/Game.tsx
+++ src/components/Game.tsx
@@ -12,19 +12,19 @@ interface Params {
 }
 
 export function Game({ league, onAddFinisher, chosenWord, finishers }: Params) {
-  // const { postText, wordCount } = useFirehose(league.words);
+  const { postText, wordCount } = useFirehose(league.words);
 
   // -- FOR DEBUGGING
-  const wordCount = {
-    one: 5,
-    two: 6,
-    three: 7,
-    four: 8,
-  };
-  const postText = {
-    word: "one",
-    text: "asdf",
-  };
+  // const wordCount = {
+  //   one: 5,
+  //   two: 6,
+  //   three: 7,
+  //   four: 8,
+  // };
+  // const postText = {
+  //   word: "one",
+  //   text: "asdf",
+  // };
   // -- END FOR DEBUGGING
 
   for (const word in wordCount) {
@@ -36,16 +36,16 @@ export function Game({ league, onAddFinisher, chosenWord, finishers }: Params) {
         <div className="post-card">{postText.text}</div>
       </div>
       <div className="all-racetrack-content">
-        {/* <Lines finishLine={league.finishLine}></Lines> */}
-        <Lines finishLine={8}></Lines>
+        <Lines finishLine={league.finishLine}></Lines>
+        {/* <Lines finishLine={8}></Lines> */}
 
         {/* Do I need the "tracks" wrapper? */}
         <div className="tracks">
           <Racetracks
             wordCount={wordCount}
             postText={postText}
-            // finishLine={league.finishLine}
-            finishLine={8}
+            finishLine={league.finishLine}
+            // finishLine={8}
             chosenWord={chosenWord}
             finishers={finishers}
           ></Racetracks>
