714855414d7d1b576d36c9adfca61317597db680
Nate May <nathaniel.p.may@gmail.com>
2025-03-05 13:28:26 -0800

    Calculate height instead of top of hose to ensure full hose is visible

--- src/components/Hose.tsx
+++ src/components/Hose.tsx
@@ -10,10 +10,7 @@ export function Hose({ progress, finishLine, finished }: Props) {
   const progressPct = finished ? 100 : (progress / finishLine) * 100;
   return (
     <div className="track-main">
-      <div
-        className="hose"
-        style={{ top: `calc(${progressPct}% - var(--hose-height))` }}
-      ></div>
+      <div className="hose" style={{ height: `${progressPct}%` }}></div>
     </div>
   );
 }
