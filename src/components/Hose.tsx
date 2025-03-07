d0f290c4395c2e2bc8600fc185de655470dcfc4e
Nate May <nathaniel.p.may@gmail.com>
2025-03-06 23:23:49 -0800

    Hoses fade out when they finish race

--- src/components/Hose.tsx
+++ src/components/Hose.tsx
@@ -10,7 +10,10 @@ export function Hose({ progress, finishLine, finished }: Props) {
   const progressPct = finished ? 100 : (progress / finishLine) * 100;
   return (
     <div className="track-main">
-      <div className="hose" style={{ height: `${progressPct}%` }}></div>
+      <div
+        className="hose"
+        style={{ height: `${progressPct}%`, opacity: finished ? 0 : 1 }}
+      ></div>
     </div>
   );
 }
