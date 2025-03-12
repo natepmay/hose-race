b5bc0976c9e62a83fc0b430fdd2418efffcecdd1
Nate May <nathaniel.p.may@gmail.com>
2025-03-11 17:55:56 -0700

    Add nozzles and make em jiggle

--- src/components/Hose.tsx
+++ src/components/Hose.tsx
@@ -1,4 +1,8 @@
 import "../App.css";
+import nozzle1 from "../assets/nozzle1-transp.png";
+import nozzle2 from "../assets/nozzle2-transp.png";
+import nozzle3 from "../assets/nozzle3-transp.png";
+import nozzle4 from "../assets/nozzle4-transp.png";
 
 interface Props {
   progress: number;
@@ -12,8 +16,22 @@ export function Hose({ progress, finishLine, finished }: Props) {
     <div className="track-main">
       <div
         className="hose"
-        style={{ height: `${progressPct}%`, opacity: finished ? 0 : 1 }}
+        style={{
+          height: `calc(${progressPct}% - 73px)`,
+          opacity: finished ? 0 : 1,
+        }}
       ></div>
+      <div className="nozzle-container">
+        <img
+          src={nozzle1}
+          width="50px"
+          className="nozzle"
+          style={{
+            bottom: `calc(100% - ${progressPct}%)`,
+            animationDelay: `${Math.random() * 5 + 2}s`,
+          }}
+        />
+      </div>
     </div>
   );
 }
