7c8d453b3a14b62f4c72000367d6ae6bde069511
Nate May <nathaniel.p.may@gmail.com>
2025-03-11 17:06:06 -0700

    Set up for debugging nozzles

--- src/components/Game.tsx
+++ src/components/Game.tsx
@@ -13,19 +13,20 @@ interface Params {
 }
 
 export function Game({ league, onAddFinisher, chosenWord, finishers }: Params) {
-  const { postText, wordCount } = useFirehose(league.words);
+  // const { postText, wordCount } = useFirehose(league.words);
 
   // -- FOR DEBUGGING
-  // const wordCount = {
-  //   one: 5,
-  //   two: 6,
-  //   three: 7,
-  //   four: 8,
-  // };
-  // const postText = {
-  //   word: "one",
-  //   text: "Hey here's a long-ass sentence with the word in it the word isss...one! ...and now the sentence continues on for a bit of a long-ass time so we can cut it off on both sides but it doesn't have to be equal on both sides for instance it's longer at the end here.",
-  // };
+  const wordCount = {
+    one: 0,
+    two: 3,
+    three: 5,
+    four: 8,
+  };
+  const postText = {
+    word: "one",
+    text: "Hey here's a long-ass sentence with the word in it the word isss...one! ...and now the sentence continues on for a bit of a long-ass time so we can cut it off on both sides but it doesn't have to be equal on both sides for instance it's longer at the end here.",
+    url: "https://www.wikipedia.org",
+  };
   // -- END FOR DEBUGGING
 
   for (const word in wordCount) {
@@ -37,16 +38,16 @@ export function Game({ league, onAddFinisher, chosenWord, finishers }: Params) {
         <PostCard postText={postText} />
       </div>
       <div className="all-racetrack-content">
-        <Lines finishLine={league.finishLine}></Lines>
-        {/* <Lines finishLine={8}></Lines> */}
+        {/* <Lines finishLine={league.finishLine}></Lines> */}
+        <Lines finishLine={8}></Lines>
 
         {/* Do I need the "tracks" wrapper? */}
         <div className="tracks">
           <Racetracks
             wordCount={wordCount}
             postText={postText}
-            finishLine={league.finishLine}
-            // finishLine={8}
+            // finishLine={league.finishLine}
+            finishLine={8}
             chosenWord={chosenWord}
             finishers={finishers}
           ></Racetracks>
